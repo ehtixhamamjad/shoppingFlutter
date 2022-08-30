affcb80b5ee980e2cb53fef7a2486e9e7dbbe533
Ehtixham Amjad <33822563+ehtixhamamjad@users.noreply.github.com>
2022-08-30 19:38:39 +0500

    day 1 commplite

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
-// git init
-// git add .
-// git commit -m first
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome to flutter'),
-          ),
-        ),
+    return MaterialApp( //use MaterialApp() widget like this
+        home: Home() //create new widget class for this 'home' to
+      // escape 'No MediaQuery widget found' error
+    );
+  }
+}
+
+//create new class for "home" property of MaterialApp()
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
       ),
+      drawer: Drawer(),
+        body: Center(
+          child: Container(
+            child: Text("Ehtisham"),
+          ),
+        )
     );
   }
 }
